07c5b1ac7fb6a78f11cfc5f5a835247c89e20485
Peter Gagliardi <pcg34@drexel.edu>
2017-12-02 12:54:15 -0500

    Fetch vertex/fragment shaders
    Now I just need to handle imports.

--- scripts/ShaderLoader.js
+++ scripts/ShaderLoader.js
@@ -2,6 +2,8 @@ class ShaderLoader {
     constructor() {
         this.config = {};
         this.library = {};
+        this.vertex_shaders = [];
+        this.fragment_shaders = [];
     }
 
     /**
@@ -11,26 +13,26 @@ class ShaderLoader {
     load() {
         return this.load_config()
             .then(() => this.load_libs())
-            .then(() => this.load_vertex_shaders())
-            .then(() => this.load_fragment_shaders())
+            .then(() => this.load_shaders())
     }
 
     /**
      * Load the main config JSON file and store it in this.config
      */
     load_config() {
-        return $.getJSON('shaders.json').then((x) => this.config = x);
+        return $.getJSON('shaders.json')
+            .then((x) => this.config = x)
     }
 
     /**
      * Load all the libraries
      */
     load_libs() {
-        // TODO: Rename to {x}.glsl
+        // TODO: Rename to shader_lib/{x}.glsl
         // Determine filenames
         let lib_urls = this.config.libs.map((x) => `shader_lib/${x}.frag`)
         // Make promises
-        let fetch_libs = lib_urls.map(ShaderLoader.shader_promise);
+        let fetch_libs = lib_urls.map(ShaderLoader.file_promise);
         // Load everything and then store them all at once.
         return Promise.all(fetch_libs).then((x) => this.store_libs(x));
     }
@@ -46,18 +48,29 @@ class ShaderLoader {
         }
     }
 
-    load_vertex_shaders() {
+    /**
+     * Load shaders
+     */
+    load_shaders() {
+        // TODO: Split into two directories
+        let vert_urls = this.config.vertex.map((x) => `shaders/${x.id}.vert`);
+        let frag_urls = this.config.fragment.map((x) => `shaders/${x.id}.frag`);
 
-    }
+        // Get promises to fetch all the files.
+        let fetch_vert = vert_urls.map(ShaderLoader.file_promise);
+        let fetch_frag = frag_urls.map(ShaderLoader.file_promise);
 
-    load_fragment_shaders() {
-
+        // Load everything and then store them all at once
+        return Promise.all(fetch_vert)
+            .then((x) => this.vertex_shaders = x)
+            .then(() => Promise.all(fetch_frag))
+            .then((x) => this.fragment_shaders = x);
     }
 
     /**
-     * Return an Ajax promise to fetch a shader from one of the folders
+     * Return an Ajax promise to fetch a plaintext file
      */
-    static shader_promise(url) {
+    static file_promise(url) {
         return $.ajax({
             url: url,
             dataType: 'text'
